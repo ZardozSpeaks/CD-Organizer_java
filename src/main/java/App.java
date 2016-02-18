3052462b54584f5dfe0ef427a2cd10f1a1ee9e4d
David Remington and Mike Gallegos <mklgallegos@gmail.com>
2016-02-18 09:42:33 -0800

    add working spark logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,6 +18,7 @@ public class App {
     get("/artists", (request,response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       model.put("artists", Artist.all());
+      model.put("albums", Album.all());
       model.put("template", "templates/artists.vtl");
       return new ModelAndView(model,layout);
     }, new VelocityTemplateEngine());
@@ -25,6 +26,12 @@ public class App {
     get("/artists/new", (request,response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/artist-form.vtl");
+      return new ModelAndView(model,layout);
+    }, new VelocityTemplateEngine());
+
+    get("/albums/new", (request,response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/album-form.vtl");
       return new ModelAndView(model,layout);
     }, new VelocityTemplateEngine());
 
@@ -36,10 +43,26 @@ public class App {
       return new ModelAndView(model, layout);
       }, new VelocityTemplateEngine());
 
+    post("/albums", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String album = request.queryParams("album");
+      Album newAlbum = new Album(album);
+      model.put("template", "templates/confirmation.vtl");
+      return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
     get("/artists/:id", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       Artist artist = Artist.find(Integer.parseInt(request.params(":id")));
       model.put("artist", artist);
+      model.put("template", "templates/artists.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/albums/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Album album = Album.find(Integer.parseInt(request.params(":id")));
+      model.put("album", album);
       model.put("template", "templates/artists.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
